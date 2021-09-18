e2981bc6851d621a9b5a7a6589f876eb6a8d844c
arpit <arpitpatel.in@gmail.com>
2021-09-18 23:02:28 +0530

    change cron run time

--- src/cron/index.js
+++ src/cron/index.js
@@ -2,8 +2,8 @@ import {CronJob} from 'cron';
 import moment from 'moment';
 import {Ride} from '../models/index.js';
 
-// Run cron at Every day 10 AM
-const removeRide = new CronJob('* 10 * * *', async () => {
+// Run cron at Every Hour
+const removeRide = new CronJob('0 * * * *', async () => {
   const time = moment().subtract(process.env.REMOVE_ABLE_HOURS, 'h');
   await Ride.deleteMany({
     updatedAt: {
